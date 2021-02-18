d5fe86c2b704fba672266749c75f8f5af44aca59
Tony <apresti12@gmail.com>
2021-02-18 14:05:24 -0700

    updates for jangomart example

--- packer/image.pkr.hcl
+++ packer/image.pkr.hcl
@@ -10,30 +10,34 @@ locals { timestamp = regex_replace(timestamp(), "[- TZ:]", "") }
 # build blocks. A build block runs provisioners and post-processors on a
 # source.
 source "amazon-ebs" "example" {
-  ami_name      = "learn-terraform-packer-${local.timestamp}"
+  ami_name      = "lamp-server-${local.timestamp}"
   instance_type = "t2.micro"
   region        = var.region
-  source_ami_filter {
-    filters = {
-      name                = "ubuntu/images/*ubuntu-xenial-16.04-amd64-server-*"
-      root-device-type    = "ebs"
-      virtualization-type = "hvm"
-    }
-    most_recent = true
-    owners      = ["099720109477"]
-  }
-  ssh_username = "ubuntu"
+  source_ami = "ami-047a51fa27710816e"
+  # source_ami_filter {
+  #   filters = {
+  #     name                = "amzn2-ami-hvm-2.0.20210126.0-x86_64-gp2"
+  #     root-device-type    = "ebs"
+  #     virtualization-type = "hvm"
+  #   }
+  #   most_recent = true
+  #   owners      = ["099720109477"]
+  # }
+  ssh_username = "ec2-user"
 }
 
 # a build block invokes sources and runs provisioning steps on them.
 build {
   sources = ["source.amazon-ebs.example"]
 
-  provisioner "file" {
-    source      = "../tf-packer.pub"
-    destination = "/tmp/tf-packer.pub"
-  }
   provisioner "shell" {
     script = "../scripts/setup.sh"
+  }
+   provisioner "shell" {
+    script = "../scripts/setup2.sh"
+  }
+  provisioner "file" {
+    source = "../app/index.html"
+    destination = "/var/www/html/index.html"
   }
 }
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,39 +1,11 @@
 #!/bin/bash
 set -x
 
-# Install necessary dependencies
-sudo apt-get update -y
-sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
-sudo apt-get update
-sudo apt-get -y -qq install curl wget git vim apt-transport-https ca-certificates
-sudo add-apt-repository ppa:longsleep/golang-backports -y
-sudo apt-get -y -qq install golang-go
+#YUM updates
+sudo yum update -y
 
-# Setup sudo to allow no-password sudo for "hashicorp" group and adding "terraform" user
-sudo groupadd -r hashicorp
-sudo useradd -m -s /bin/bash terraform
-sudo usermod -a -G hashicorp terraform
-sudo cp /etc/sudoers /etc/sudoers.orig
-echo "terraform  ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/terraform
-
-# Installing SSH key
-sudo mkdir -p /home/terraform/.ssh
-sudo chmod 700 /home/terraform/.ssh
-sudo cp /tmp/tf-packer.pub /home/terraform/.ssh/authorized_keys
-sudo chmod 600 /home/terraform/.ssh/authorized_keys
-sudo chown -R terraform /home/terraform/.ssh
-sudo usermod --shell /bin/bash terraform
-
-# Create GOPATH for Terraform user & download the webapp from github
-
-sudo -H -i -u terraform -- env bash << EOF
-whoami
-echo ~terraform
-
-cd /home/terraform
-
-export GOROOT=/usr/lib/go
-export GOPATH=/home/terraform/go
-export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
-go get -d github.com/hashicorp/learn-go-webapp-demo
-EOF
+sudo yum install -y udo amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
+sudo yum install -y httpd mariadb-server
+sudo systemctl start httpd
+sudo chkconfig httpd on
+sudo usermod -a -G apache ec2-user
